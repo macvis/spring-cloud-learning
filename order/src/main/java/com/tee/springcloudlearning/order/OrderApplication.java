9b634831d49ccba7b6e95122e5ee44b4698af348
温友朝 <wb.wenyouchao@mesg.corp.netease.com>
2018-11-08 11:44:01 +0800

    代码暂存

---
+++ order/src/main/java/com/tee/springcloudlearning/order/OrderApplication.java
@@ -0,0 +1,12 @@
+package com.tee.springcloudlearning.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
